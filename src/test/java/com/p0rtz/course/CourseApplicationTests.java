6d391ec545f9d2e5ccef4e9d3a33dc7646a7d9c2
EduardPortes <epcduas05@hotmail.com>
2024-08-30 18:57:06 -0300

    Projct created

---
+++ src/test/java/com/p0rtz/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p0rtz.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
