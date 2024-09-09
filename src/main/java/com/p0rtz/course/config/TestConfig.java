a493c9e0e5b5aa4eec892c48abc5b14806e51b8d
EduardPortes <epcduas05@hotmail.com>
2024-09-09 15:33:45 -0300

    OrderStatus enum

--- src/main/java/com/p0rtz/course/config/TestConfig.java
+++ src/main/java/com/p0rtz/course/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.p0rtz.course.config;
 
 import com.p0rtz.course.entities.Order;
 import com.p0rtz.course.entities.User;
+import com.p0rtz.course.entities.enums.OrderStatus;
 import com.p0rtz.course.repositories.OrderRepository;
 import com.p0rtz.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "9888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "9777777", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10z"), OrderStatus.WAITING_PAYMENT,u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22z"), OrderStatus.WAITING_PAYMENT,u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
--- src/main/java/com/p0rtz/course/entities/Order.java
+++ src/main/java/com/p0rtz/course/entities/Order.java
@@ -2,7 +2,9 @@ package com.p0rtz.course.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.p0rtz.course.entities.enums.OrderStatus;
 import jakarta.persistence.*;
+import org.antlr.v4.runtime.atn.OrderedATNConfigSet;
 
 import java.time.Instant;
 import java.util.Objects;
@@ -18,6 +20,7 @@ public class Order {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -26,9 +29,10 @@ public class Order {
     public Order(){
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus,User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -46,6 +50,16 @@ public class Order {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
