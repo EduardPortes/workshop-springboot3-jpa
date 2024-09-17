7687009cb3d09bfa27743f34ff3462a7b856df3b
EduardPortes <epcduas05@hotmail.com>
2024-09-17 14:54:49 -0300

    User Delete

--- src/main/java/com/p0rtz/course/resources/UserResource.java
+++ src/main/java/com/p0rtz/course/resources/UserResource.java
@@ -3,6 +3,7 @@ package com.p0rtz.course.resources;
 import com.p0rtz.course.entities.User;
 import com.p0rtz.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -36,6 +37,11 @@ public class UserResource {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                  .buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
+     }
+     @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
      }
 
 }
