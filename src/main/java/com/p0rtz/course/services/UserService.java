7d6f530152c82efc7a8bc3726625a75b7b31127b
EduardPortes <epcduas05@hotmail.com>
2024-09-18 11:57:37 -0300

    Exception handling - update

--- src/main/java/com/p0rtz/course/services/UserService.java
+++ src/main/java/com/p0rtz/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.p0rtz.course.entities.User;
 import com.p0rtz.course.repositories.UserRepository;
 import com.p0rtz.course.services.exceptions.DatabaseException;
 import com.p0rtz.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -49,9 +50,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
 }
